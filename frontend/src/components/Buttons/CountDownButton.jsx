d5d17611a6fedc129f8664dd45e5dc0c94f5371d
githab user name <bangtang0914@gmail.com>
2021-07-27 20:55:36 +0900

    first upload

---
+++ frontend/src/components/Buttons/CountDownButton.jsx
@@ -0,0 +1,12 @@
+// --- ここから追加 ---
+import React from "react";
+
+// style
+import { RoundButton } from "../shared_style";
+
+export const CountDownButton = ({ onClick, isDisabled }) => (
+  <RoundButton onClick={onClick} disabled={isDisabled}>
+    ー
+  </RoundButton>
+);
+// --- ここまで追加 ---
